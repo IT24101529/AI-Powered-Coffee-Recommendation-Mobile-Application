9e2fa22553f7be58a5aaa6d62e9bbbea87329ab6
Nalina Ranasinghe <it24101529@my.sliit.lk>
2026-04-02 10:56:18 +0530

    api update

--- EmberCoffeeCo/src/config/api.js
+++ EmberCoffeeCo/src/config/api.js
@@ -1,4 +1,4 @@
 const DEV_URL = 'http://192.168.x.x:5000';
-const PROD_URL = 'https://ember-coffee-api.onrender.com';
+const PROD_URL = 'embercoffeeco-a-coffee-shop-mobile-applicat-production.up.railway.app';
 
 export const BASE_URL = __DEV__ ? DEV_URL : PROD_URL;
